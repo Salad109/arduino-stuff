609d7f2febec74f9d6e2d472a36466812835bf55
Salad <47259339+Salad109@users.noreply.github.com>
2024-03-05 17:46:32 +0100

    Add files via upload

--- wireless-weather-station/Transmitter.ino
+++ wireless-weather-station/Transmitter.ino
@@ -15,22 +15,21 @@ MISO  -> 12
 SCK   -> 13 
 IRQ   -> N/C
 GND   -> GND
-VCC                -> Transistor collector
-Transistor base    -> D8
-Transistor emitter -> 3V3
+VCC   -> 3V3
 ============================================================
 DFRobot Solar Power Manager    Arduino
 EN=BAT
 5V                          -> 5V
 GND                         -> GND
 ==========================================================*/
-#include "forcedClimate.h"
+#include <forcedClimate.h>
 #include "LowPower.h"
 #include "NRFLite.h"
+#include "avr/power.h"
 
-#define DEBUG_MODE 1     // 1 = Serial debugging messages, 0 = Serial disabled
+#define DEBUG_MODE 1  // 1 = Serial debugging messages, 0 = Serial disabled
 
-ForcedClimate climateSensor = ForcedClimate(Wire, 0x76);
+ForcedClimate climateSensor = ForcedClimate(Wire, 0x76);  //  CS low = 0x76, CS high = 0x77
 
 
 const static uint8_t RADIO_ID = 1;              // TX RADIO ID(me)
@@ -52,10 +51,13 @@ NRFLite _radio;
 RadioPacket _radioData;
 
 void setup() {
+  disableStuff();  // Various modules which are never used
+
   if (DEBUG_MODE) {
     Serial.begin(9600);
     Serial.println("Hello world");
   }
+
   Wire.begin();
   climateSensor.begin();
 
@@ -67,9 +69,9 @@ void setup() {
 }
 
 void loop() {
-  getData();                          // Get readings then save them to the _radioData object.
-  _radioData.OnTimeMillis = millis();
-  if (DEBUG_MODE) {  // DEBUGGING LOOP
+  getData();  // Get readings then save them to our _radioData object
+
+  if (DEBUG_MODE) {  // Debugging loop
     printData();
 
     Serial.print("Sending ");
@@ -82,19 +84,13 @@ void loop() {
       Serial.println("...Failed");
       _radioData.FailedTxCount++;
     }
-  } else {  // STEALTH LOOP
+  } else {  // Stealth loop
     if (!_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) {
       _radioData.FailedTxCount++;
     }
   }
 
-  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
-  /*
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
-  */
+  goToSleep();
 }
 
 //===========================================================
@@ -117,4 +113,57 @@ void getData() {
   _radioData.temp = climateSensor.getTemperatureCelcius();
   _radioData.pres = climateSensor.getPressure();
   _radioData.humi = climateSensor.getRelativeHumidity();
+  _radioData.OnTimeMillis = millis();
+}
+
+void disableStuff() {
+  for (int i = 0; i < 20; i++) {  // Set all unused pins as low outputs
+    if (i != 4 && i != 5 && (i < 9 || i > 13)) {
+      pinMode(i, OUTPUT);
+      digitalWrite(i, LOW);
+    }
+  }
+  MCUCR = bit(BODS) | bit(BODSE);  // Disable brown-out detector
+  MCUCR = bit(BODS);               //
+
+  ADCSRA = 0;              // Disable ADC
+  ADCSRA &= ~(1 << ADEN);  //
+
+  ACSR = (1 << ACD);  // Disable ACD
+
+  DIDR0 = 0x3F;                         // ?
+  DIDR1 = (1 << AIN1D) | (1 << AIN0D);  //
+}
+
+void goToSleep() {
+  power_all_disable();
+  power_twi_disable();     // I2C
+  power_usart0_disable();  // Serial
+
+
+  power_spi_disable();       // SPI
+  byte old_ADCSRA = ADCSRA;  //
+  ADCSRA = 0;                //
+
+  //power_timer0_disable();  // Needed for delay and millis()
+  //power_timer1_disable();  //
+  //power_timer2_disable();  // Needed for asynchronous 32kHz operation
+
+  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+  /*
+  for (int i = 0; i < 8; i++) {                      // 64s wakeup interval
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  //
+  }                                                  //
+  */
+
+  power_all_enable();
+  power_twi_enable();      // I2C
+  power_usart0_disable();  // Serial
+
+  ADCSRA = old_ADCSRA;  // SPI
+  power_spi_enable();   //
+
+  //power_timer0_enable();
+  //power_timer1_enable();
+  //power_timer2_enable();
 }
